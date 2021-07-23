90881746183affff8c2ae7a1a5bffffc4470cb63
Proshanta Debnath <debnathproshanta80@gmail.com>
2021-07-23 21:24:30 +0530

    intial commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+const questions = [
+    {
+    numb: 1,
+    question: "Database is generally __________",
+    answer: "b)User-centered",
+    options: [
+      "a)System-centered",
+      "b)User-centered",
+      "c)Company-centered",
+      "d)Data-centered"
+    ]
+  },
+    {
+    numb: 2,
+    question: "A characteristic of an entity.",
+    answer: "b)Attribute",
+    options: [
+      "a)Relation",
+      "b)Attribute",
+      "c)Parameter",
+      "d)Constraint"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
